541de1126057117cfd23cd88f79ebf5043eee551
Eric Bai <ebai@uwaterloo.ca>
2018-08-03 23:42:27 -0400

    add css, edit words in intro

--- index.jsx
+++ index.jsx
@@ -2,20 +2,21 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 
 ReactDOM.render(
-  <div>
+  <div class="chapter">
     <h1>The Science of Colour</h1>
     <p>
-      In 2015, the Internet furiously debated over the colour of a dress found in this infamous photo.
+      In 2015, the Internet furiously debated over the colour of a dress found in this viral photo.
       Is the dress white and gold, or black and blue?
     </p>
     <img src="assets/dress.png" alt="The dress"></img>
     <p>
-      This simple question went viral.
-      It turns out the dress is actually black and blue... but how was everyone so evenly divided on the colour?
+      This meme was so popular that it's simply referred to as "the dress".
+      While it turned out the dress is actually black and blue, the even divide between #BlackAndBlue and #WhiteAndGold showed us something.
+      Under certain conditions, black can look like gold, and blue can look like white.
+      ...How exactly is that possible?
     </p>
     <p>
-      How we perceive colour is based on a complex combination of the properties of the lighting, the subject, and our brains.
-      The following is an interactive explanation of how colour works.
+      This post explains the science behind how colour exists and is perceived, and by the end you'll understand how BLACK = GOLD and BLUE = WHITE.
     </p>
   </div>,
   document.getElementById("root")
