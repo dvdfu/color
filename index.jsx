49a296fc9a6e9294c2d4f8eeedc4a559a430fa32
Eric Bai <ebai@uwaterloo.ca>
2018-08-05 14:42:11 -0400

    start act 1

--- index.jsx
+++ index.jsx
@@ -5,7 +5,7 @@ ReactDOM.render(
   <div class="chapter">
     <h1>The Science of Colour</h1>
     <p>
-      In 2015, the Internet furiously debated over the colour of a dress found in this viral photo.
+      In 2015, the Internet furiously debated about an image of a dress.
       Is the dress white and gold, or black and blue?
     </p>
     <img src="assets/dress.png" alt="The dress"></img>
@@ -18,6 +18,24 @@ ReactDOM.render(
     <p>
       This post explains the science behind how colour exists and is perceived, and by the end you'll understand how BLACK = GOLD and BLUE = WHITE.
     </p>
+    <h2>Light</h2>
+    <p>
+      Without light, there wouldn’t be any colour to see. So let’s start with how exactly light works.
+    </p>
+    <p>
+      Light is a type of energy that travels in a wave.
+      The <strong>wavelength</strong> describes how much energy it carries; smaller wavelengths mean higher energy.
+    </p>
+    [EM Spectrum table that shows wavelengths and common names for them]
+    <p>
+      Our eyes can detect light with wavelengths around 390-700nm, so we call this range of the light the <strong>visible spectrum</strong>.
+      There are plenty of other wavelengths we can’t see, like microwaves, radio waves, and X-rays, because our eyes don’t react to them.
+    </p>
+    <p>
+      Within the visible spectrum, the wavelength defines which colour we see!
+      Try to find the wavelengths that correspond to the following colours:
+    </p>
+
   </div>,
   document.getElementById("root")
 );
