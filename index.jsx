0fd91777d9d3ca6bc65ac4f3483aa777d83da21f
dvdfu <davidf1212@gmail.com>
2018-08-05 15:14:36 -0400

    cleanup

--- index.jsx
+++ index.jsx
@@ -1,23 +1,41 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+const React = require("react");
+const ReactDOM = require("react-dom");
 
-ReactDOM.render(
-  <div class="chapter">
+const chapters = [];
+
+chapters[0] = (
+  <div className="chapter">
     <h1>The Science of Colour</h1>
+  </div>
+);
+
+chapters[1] = (
+  <div className="chapter">
+    <h2>Huh???</h2>
     <p>
-      In 2015, the Internet furiously debated over the colour of a dress found in this viral photo.
-      Is the dress white and gold, or black and blue?
+      In 2015, the Internet furiously debated over the colour of a dress found
+      in this viral photo. Is the dress white and gold, or black and blue?
     </p>
-    <img src="assets/dress.png" alt="The dress"></img>
+    <img src="assets/dress.png" alt="The dress" />
     <p>
       This meme was so popular that it's simply referred to as "the dress".
-      While it turned out the dress is actually black and blue, the even divide between #BlackAndBlue and #WhiteAndGold showed us something.
-      Under certain conditions, black can look like gold, and blue can look like white.
-      ...How exactly is that possible?
+      While it turned out the dress is actually black and blue, the even divide
+      between #BlackAndBlue and #WhiteAndGold showed us something. Under certain
+      conditions, black can look like gold, and blue can look like white. ...How
+      exactly is that possible?
     </p>
     <p>
-      This post explains the science behind how colour exists and is perceived, and by the end you'll understand how BLACK = GOLD and BLUE = WHITE.
+      This post explains the science behind how colour exists and is perceived,
+      and by the end you'll understand how BLACK = GOLD and BLUE = WHITE.
     </p>
-  </div>,
-  document.getElementById("root")
+  </div>
 );
+
+chapters[2] = (
+  <div className="chapter">
+    <h2>Light as a Wave</h2>
+    <p>It's true</p>
+  </div>
+);
+
+ReactDOM.render(chapters, document.getElementById("root"));
